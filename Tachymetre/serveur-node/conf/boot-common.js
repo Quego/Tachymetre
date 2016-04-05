5d55b10d575e45572f9bb6f561c59ab8618be22a
Quentin <quentinmace@hotmail.com>
2016-04-05 17:08:31 +0200

    maj

--- Tachymetre/serveur-node/conf/boot-common.js
+++ Tachymetre/serveur-node/conf/boot-common.js
@@ -3,7 +3,7 @@
     "import-files" : [ "boot-common.js" ],
     "properties" : {
         
-    "herald.application.id" : "tachymetre2",
+    "herald.application.id" : "tachymetre",
     "cohorte.node.data.dir" : "/home/quego/Documents/Project/Tachymetre/Tachymetre/serveur-node/data"
 
     }
